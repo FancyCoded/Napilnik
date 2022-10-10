3b1ebe6459e9df71a5dd0614b0fc25313891f50b
FancyCoded <sabyrbekovjanbolot@gmail.com>
2022-10-10 18:46:02 +0600

    task11

--- task11.cs
+++ task11.cs
@@ -8,7 +8,7 @@ public static void GenerateChance()
     _chance = Random.Range(0, 100);
 }
 
-public static int SetSalary(int hoursWorked)
+public static int GetSalary(int hoursWorked)
 {
     return _hourlyRate * hoursWorked;
 }
