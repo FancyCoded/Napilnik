0187ddc9a21f95648756846d22a82632c5462c26
Janbolot Sabyrbekov <99344306+FancyCoded@users.noreply.github.com>
2022-10-11 15:00:48 +0600

    some revision

--- task13.cs
+++ task13.cs
@@ -19,20 +19,23 @@
     {
         string connectionString = string.Format("Data Source=" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\db.sqlite");
         SQLiteConnection connection = new SQLiteConnection(connectionString);
+        DataTable dataTable = new DataTable();
         
         connection.Open();
-        FindPassportInDataTable(connection);
+        
+        FindPassportInDataTable(connection, DataTable dataTable);
+        SetResult(dataTable);
+        
         connection.Close();
     }
     catch (SQLiteException exeption)
         TryHandle(exeption);
 }
 
-private void FindPassportInDataTable(SQLiteConnection connection)
+private void FindPassportInDataTable(SQLiteConnection connection, DataTable dataTable)
 {
     string commandText = string.Format("select * from passports where num='{0}' limit 1;", (object)Form1.ComputeSha256Hash(rawData));
     SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(new SQLiteCommand(commandText, connection));
-    DataTable dataTable = new DataTable();
 
     sqLiteDataAdapter.Fill(dataTable);
     
@@ -41,11 +44,9 @@ private void FindPassportInDataTable(SQLiteConnection connection)
         this.textResult.Text = "Паспорт «" + this.passportTextbox.Text + "» в списке участников дистанционного голосования НЕ НАЙДЕН";
         return;
     }
-
-    SetResult();
 }
 
-private void SetResult()
+private void SetResult(DataTable dataTable)
 {
     if (Convert.ToBoolean(dataTable.Rows[0].ItemArray[1]))
         this.textResult.Text = "По паспорту «" + this.passportTextbox.Text + "» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
