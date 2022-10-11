155603f7a2d9fde3d44956814945dd301b323565
Janbolot Sabyrbekov <99344306+FancyCoded@users.noreply.github.com>
2022-10-11 20:05:00 +0600

    Update task7.cs

--- task7.cs
+++ task7.cs
@@ -1,11 +1,11 @@
 class Weapon
 {
     private const int EmptyMagazineValue = 0;
-    private const int NumberOfBulletsShooted = 1;
+    private const int BulletsPerShoot = 1;
 
     private int _bullets;
 
     public bool CanShoot() => _bullets > EmptyMagazineValue;
 
-    public void Shoot() => _bullets -= NumberOfBulletsShooted;
+    public void Shoot() => _bullets -= BulletsPerShoot;
 }
